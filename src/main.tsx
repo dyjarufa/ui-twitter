11e6669d319b0d2cde2b6036c2dd467a965e2a26
Jady Rufino <jadyrufa@gmail.com>
2023-11-09 20:48:31 +0000

    feat: add tweet list layout

--- src/main.tsx
+++ src/main.tsx
@@ -8,12 +8,14 @@ import {
   FileText,
   Hash,
   House,
+  Sparkle,
   User,
 } from 'phosphor-react'
 
 import './globals.css'
 
 import logoTwitter from './assets/logo-twitter.svg'
+import { Tweet } from './components/Tweet'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -60,7 +62,30 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           Tweet
         </button>
       </aside>
-      <div className="content">content</div>
+      <div className="content">
+        <main className="timeline">
+          <div className="timeline-header">
+            Home
+            <Sparkle />
+          </div>
+
+          <form className="new-tweet-form">
+            <label htmlFor="tweet">
+              <img src="https://github.com/dyjarufa.png" alt="Jady Rufino" />
+              <textarea id="tweet" placeholder="What's happening?" />
+            </label>
+
+            <button type="submit">Tweet</button>
+          </form>
+
+          <div className="separator" />
+
+          <Tweet />
+          <Tweet />
+          <Tweet />
+          <Tweet />
+        </main>
+      </div>
     </div>
   </React.StrictMode>
 )
